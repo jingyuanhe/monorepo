3137bd3d2b01e27513c01ced2ad777cf1d463ce6
jingyuanhe <389026947@qq.com>
2023-08-06 16:49:34 +0800

    release: vxwg-cli@0.0.20

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,25 @@
+const types = [
+  "feat",
+  "fix",
+  "docs",
+  "style",
+  "refactor",
+  "perf",
+  "test",
+  "build",
+  "release",
+  "chore",
+  "revert",
+];
+
 module.exports = {
-  extends: [
-    '@commitlint/config-conventional',
-  ],
+  extends: ["@commitlint/config-conventional"],
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'style', 'ci', 'refactor', 'perf', 'test', 'build', 'chore', 'revert', 'release']],
-    'subject-max-length': [1, 'always', 150],
+    "type-empty": [2, "never"],
+    "type-enum": [2, "always", types],
+    "scope-case": [0, "always"],
+    "subject-empty": [2, "never"],
+    "subject-case": [0, "never"],
+    "header-max-length": [2, "always", 88],
   },
-}
+};
